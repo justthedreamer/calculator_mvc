6a9890640a58a50befd1160bfb824aa1b6d0a3d7
Piotr Zapala <piotr.zapala.dev@gmail.com>
2023-10-02 20:50:03 +0200

    Calculator

---
+++ Kalulator/Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalulator.Models;
+
+public class Calculator
+{
+    public double A { get; set; }
+
+    public double B { get; set; }
+
+    public double Result { get; set; }
+}
---
+++ Kalulator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Kalulator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
